31d8e3499a0cbb78b5b500bd15f1ae55acd84902
NightsProject <nightnightproject@gmail.com>
2025-10-30 22:55:35 +0800

    fix(modify): modified the parameter named rows and cols to accept inputs

--- hard-fork/src/runner.py
+++ hard-fork/src/runner.py
@@ -3,7 +3,7 @@ import time
 from grid_generation_module import make_grid, print_grid
 from step import step
 
-def run(rows=20, cols=60, alive_prob=0.2, steps=None, interval=0.1, wrap=True, seed=None):
+def run(rows=None, cols=None, alive_prob=0.2, steps=None, interval=0.1, wrap=True, seed=None):
    
     if seed is not None:
         random.seed(seed)                            
