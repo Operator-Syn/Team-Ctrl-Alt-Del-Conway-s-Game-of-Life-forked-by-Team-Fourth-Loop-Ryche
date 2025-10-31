361a81d02d628c2ff3f503b8004f33a6d67c0b5e
Operator-Syn <johnronanbeira100404@gmail.com>
2025-10-31 13:31:26 +0800

    Created second test

--- hard-fork/test/test_count_neighbors.py
+++ hard-fork/test/test_count_neighbors.py
@@ -8,3 +8,12 @@ def test_no_neighbors():
         [0, 0, 0]
     ]
     assert count_neighbors(grid, 1, 1, wrap=False) == 0
+
+def test_full_neighbors():
+    grid = [
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1]
+    ]
+    # The center cell should have 8 neighbors
+    assert count_neighbors(grid, 1, 1, wrap=False) == 8
